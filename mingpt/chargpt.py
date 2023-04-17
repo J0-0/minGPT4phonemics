667a36bc268d49275ad790aa2f345dc9ef7cfece
J0-0 <josephine.raugel@mines-paristech.fr>
2023-04-17 18:39:10 +0200

    phonemes api

--- mingpt/chargpt.py
+++ mingpt/chargpt.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
     set_seed(config.system.seed)
 
     # construct the training dataset
-    text = open('/content/minGPT4phonemics/mingpt/wiki_train_to_phonemes.txt', 'r').read() # don't worry we won't run out of file handles
+    text = open('/content/minGPT4phonemics/mingpt/wiki_train_to_phonemes.txt', 'r').read()
     train_dataset = CharDataset(config.data, text)
     # print("DICTIONNARY ", train_dataset.itos)
 
