250b5a13c54130b979d8d3b10fd57c1bb3d8ec6f
J0-0 <josephine.raugel@mines-paristech.fr>
2023-04-17 17:35:52 +0200

    phonemes api

--- mingpt/chargpt.py
+++ mingpt/chargpt.py
@@ -121,7 +121,7 @@ if __name__ == '__main__':
                 x = torch.tensor([train_dataset.stoi[s] for s in context], dtype=torch.long)[None,...].to(trainer.device)
                 y = model.generate(x, 500, temperature=1.0, do_sample=True, top_k=10)[0]
                 completion = ''.join([train_dataset.itos[int(i)] for i in y])
-                print("COMPLETION ", completion)
+                print(completion)
             # chars_computes = sorted(list(set(data)))
             # self.itos = { i:ch for i,ch in enumerate(chars) } 
             # train_dataset.itos[int(idx_next)],
